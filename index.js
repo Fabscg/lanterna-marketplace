912b38fde803c0f7298c79f20a0ae8ab78c77cc1
Fabulosa <fabiscg79@gmail.com>
2022-07-05 13:39:19 -0400

    finished site

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-$("button").on('click', function(){
-    var panelId = $(this).attr('data-content');
-    $('#'+panelId).toggle(500);
-
-    $(this).find('.panel-icon').text(function(_, txt) {
-        return txt === "+" ? "-" : "+";
+$(document).ready(function () {
+    // Add minus icon for collapse element which is open by default
+    $(".collapse.show").each(function () {
+        $(this)
+            .prev(".accordion-header")
+            .find(".fa-solid")
+            .addClass("fa-minus")
+            .removeClass("fa-plus");
     });
+
+    // Toggle plus minus icon on show hide of collapse element
+    $(".collapse")
+        .on("show.bs.collapse", function () {
+            $(this)
+                .prev(".accordion-header")
+                .find(".fa-solid")
+                .removeClass("fa-plus")
+                .addClass("fa-minus");
+        })
+        .on("hide.bs.collapse", function () {
+            $(this)
+                .prev(".accordion-header")
+                .find(".fa-solid")
+                .removeClass("fa-minus")
+                .addClass("fa-plus");
+        });
 });
