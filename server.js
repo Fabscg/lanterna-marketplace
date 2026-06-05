abac4c0de3dc8d3d9faeb71026337cb1496c8bba
Fabiola Castaneda <fabiscg79@gmail.com>
2026-06-05 15:56:46 -0400

    npthing to commit

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // Instead of:
-import { getAnalytics } from "firebase/analytics";
-
 // Use this:
 import { getAnalytics, isSupported } from "firebase/analytics";
 
@@ -25,5 +23,3 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-// const analytics = getAnalytics(app);
